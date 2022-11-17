f2dcd8f40ece5156d35ffb98ac7a7a46bbdb551c
zhewei <zhangxiaoyuan@uni-ubi.com>
2022-11-17 16:02:15 +0800

    feat: add i18n-lib

---
+++ packages/i18n-lib/src/lib/i18n-lib.spec.ts
@@ -0,0 +1,7 @@
+import { i18nLib } from './i18n-lib';
+
+describe('i18nLib', () => {
+  it('should work', () => {
+    expect(i18nLib()).toEqual('i18n-lib');
+  });
+});
---
+++ packages/i18n-lib/src/lib/i18n-lib.ts
@@ -0,0 +1,3 @@
+export function i18nLib(): string {
+  return 'i18n-lib';
+}
